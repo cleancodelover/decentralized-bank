8a69d241a067ad1e94b4677f77f53ef50b2e13e2
FDC_DEV <onah.harry@gmail.com>
2021-05-13 11:23:45 +0100

    First commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,21 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+    address public minter;
 
-  //add minter changed event
+    //add minter variable
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    //add minter changed event
+    event MinterChanged(address indexed from, address to);
 
-  //Add pass minter role function
+    constructor() public payable ERC20() {
+        //asign initial minter
+        minter = msg.sender;
+    }
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
-	}
+    //Add pass minter role function
+    function passMinterRole(address dBank) public returns (bool) {
+        require(
+            msg.sender == minter,
+            "Error, only owners can change pass minter role"
+        );
+        minter = dBank;
+
+        emit MinterChanged(msg.sender, dBank);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) public {
+        //check if msg.sender have minter role
+        require(
+            msg.sender == minter,
+            "Error, msg.sender does not have minter role"
+        );
+        _mint(account, amount);
+    }
 }
